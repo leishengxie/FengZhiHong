16810c4bbdd5f486c2b705ff00b6762526b5d389
Lx <654380976@qq.com>
2019-12-22 23:41:23 +0800

    1

--- LxDairy/src/RegisterDialog.cpp
+++ LxDairy/src/RegisterDialog.cpp
@@ -59,22 +59,22 @@ void CRegisterDialog::on_btnOK_clicked()
 
 void CRegisterDialog::on_btnRegister_clicked()
 {
-    QString strUserName = ui->leUserName->text();
-    QString strPasswd = ui->lePasswd->text();
-    QString strPasswdSure = ui->lePasswdSure->text();
-    if(strUserName.isEmpty() || strPasswd.isEmpty())
-    {
-        QMessageBox::information(this, "ERROR", "未填写完整！请全部填写完整");
-        return;
-    }
-    else
-    {
-        if(strPasswd != strPasswdSure)
-        {
-            QMessageBox::information(this, "ERROR", "两次填写密码不一致！请检查重写！！");
-            return;
-        }
-    }
+//    QString strUserName = ui->leUserName->text();
+//    QString strPasswd = ui->lePasswd->text();
+//    QString strPasswdSure = ui->lePasswdSure->text();
+//    if(strUserName.isEmpty() || strPasswd.isEmpty())
+//    {
+//        QMessageBox::information(this, "ERROR", "未填写完整！请全部填写完整");
+//        return;
+//    }
+//    else
+//    {
+//        if(strPasswd != strPasswdSure)
+//        {
+//            QMessageBox::information(this, "ERROR", "两次填写密码不一致！请检查重写！！");
+//            return;
+//        }
+//    }
 
 
       int nResult = CSqlOperate::registerAccount(strUserName, strPasswd);
@@ -89,6 +89,7 @@ void CRegisterDialog::on_btnRegister_clicked()
           return;
       }
       QMessageBox::information(this, "OK", QString("账号为：%1\n密码：%2\n请记住你的账号和密码！\n").arg(strUserName).arg(strPasswd));
+
       accept();
 
 }
---
+++ LxDairy/src/src_net_appointments.pri
@@ -0,0 +1,33 @@
+
+#may be these condition about loading files is not used.
+DEFINES += APPOINTMENT_DATASTREAM
+#DEFINES += APPOINTMENT_JSON
+#DEFINES += APPOINTMENT_PROTOBUFF
+
+#DEFINES += APPOINTMENT_TYPE = APPOINTMENT_DATASTREAM
+
+INCLUDEPATH += $$PWD
+INCLUDEPATH += $$PWD/net_appointments
+INCLUDEPATH += $$PWD/module_collection
+
+HEADERS += $$PWD/net_appointments/NetAppointments.h \
+    $$PWD/module_collection/collection_jokebook/Joke.h
+SOURCES += $$PWD/net_appointments/NetAppointments.cpp \
+    $$PWD/module_collection/collection_jokebook/Joke.cpp
+
+contains(DEFINES, APPOINTMENT_DATASTREAM){
+        INCLUDEPATH += $$PWD/net_appointments/datastream_type
+        HEADERS += $$PWD/net_appointments/datastream_type/NetDataStream.h
+}
+
+#contains(DEFINES, APPOINTMENT_JSON){
+#        INCLUDEPATH += $$PWD/json_type
+#        HEADERS += $$PWD/json_type/NetJson.h
+#}
+
+#contains(DEFINES, APPOINTMENT_PROTOBUFF){
+#        INCLUDEPATH += $$PWD/protobuff_type
+#        HEADERS += $$PWD/protobuff_type/NetProtobuff.h
+#}
+
+
