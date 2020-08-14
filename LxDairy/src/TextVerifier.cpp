3448ec7e2a7db73c437c4a5049d7b6d9ddce8c94
leishengxie <654380976@qq.com>
2020-08-14 18:36:32 +0800

    2

--- LxDairy/src/TextVerifier.cpp
+++ LxDairy/src/TextVerifier.cpp
@@ -4,3 +4,36 @@ CTextVerifier::CTextVerifier(QObject *parent) : QObject(parent)
 {
 
 }
+
+
+///
+/// \brief Palindate::Palindate 回文验证
+/// \param parent
+///
+Palindate::Palindate(QObject *parent)
+    : QValidator(parent)
+{
+
+}
+
+QValidator::State Palindate::validate(QString &strInput, int &) const
+{
+    QString strInputLower(strInput.toLower());
+    QString strSkip("-_!,k. \t");
+    foreach (QChar ch, strSkip) {
+        strInputLower = strInputLower.remove(ch);
+    }
+    QString strRev;
+    for (int i = strInput.length(); i > 0; --i)
+    {
+        strRev.append(strInput[i - 1]);
+    }
+    if (strInput == strRev)
+    {
+        return Acceptable;
+    }
+    else
+    {
+        return Intermediate;
+    }
+}
--- LxDairy/src/TextVerifier.h
+++ LxDairy/src/TextVerifier.h
@@ -2,10 +2,13 @@
 #define CTEXTVERIFIER_H
 
 #include <QObject>
+#include <QValidator>
 
 
 ///
 /// \brief The CTextVerifier class @用于验证文本的合法性
+/// 1.掩码 mask input, 针对QLineEdit setInputMask, 主要验证哪个位置的何种字符是允许的。
+/// 2.验证器 validator 针对QLineEdit setValidator, 包含数字，正则表达式等
 ///
 class CTextVerifier : public QObject
 {
@@ -16,9 +19,20 @@ public:
 public:
     static bool isValidPasswd(QString strPasswd, unsigned int unMaxLength, bool bCkeckP);
 
+    QString phoneMask() const;
+
 signals:
 
 public slots:
+};
+
+// 回文字符串验证
+class Palindate : public QValidator
+{
+    Q_OBJECT
+public:
+    explicit Palindate(QObject* parent = 0);
+    QValidator::State validate(QString & strInput, int &) const;
 };
 
 #endif // CTEXTVERIFIER_H
